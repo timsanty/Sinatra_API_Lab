5fa17a4dde04eff5907a8ef92e74852cd75defad
timsanty <timsanty@me.com>
2014-04-17 21:38:57 -0700

    movie app

--- movies.rb
+++ movies.rb
@@ -4,12 +4,37 @@ require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
 
-get '/' do
+
+
+get '/' do <<EOS
+	<h1> Movie Search!</h1>
+		<form action = "/results" method = "get" />
+		<input type = "text" name="movies" />
+		<input type = "submit" />
+EOS
 end
 
 get '/results' do
+	if params["movie"] == ""
+		redirect '/'
+	end
+
+	response = Typhoeus.get("http://www.omdbapi.com/", :params => { :s => params["movie"] })
+	result = JSON.parse(response.body)
+	str = ""
+
+	result["Search"].each do |movie|
+		str += "<br><a href=poster/#{movie["imdbID"]}> #{movie["Title"]} - #{movie["Year"]} </a><br>"""
+	end
+		str
 end
 
-# TODO: Add another get here for the poster url.  The path for the poster
-# should look like this example '/poster/tt2724064'
+
+get '/poster/:imdbID' do
+response = Typhoeus.get("http://www.omdbapi.com/", :params => { :i => params["imdbID"]})
+movie_poster = JSON.parse(response.body)
+"<img src = #{movie_poster["Poster"]} />"
+
+end
+
 
